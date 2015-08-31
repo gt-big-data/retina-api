4d12f91efbfa1066fa6d468bdaf198ccacdbdfae
Patrick Violette <violette.patrick718@gmail.com>
2015-08-30 22:32:54 -0400

    Added run script

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import argparse
+
 from flask import Flask
 from dbco import *
 app = Flask(__name__)
@@ -8,7 +10,24 @@ def hello_world():
 
 @app.route('/ask')
 def getRecentArticles():
-	return 'Miaw'
+    return 'Miaw'
 
+def isProdEnvironment():
+   '''
+   Read the mode from command line flags.
+   
+   Returns True if flag prod is set, False otherwise.
+   '''
+   parser = argparse.ArgumentParser(description='Flags for API server.')
+   parser.add_argument('prod', type=bool, nargs=1, default=False)
+   args = parser.parse_args()
+   return args.prod
+
+def main():
+    if isProdEnvironment():
+        app.run(host='0.0.0.0', port=80)
+    else:
+        app.run()
+   
 if __name__ == '__main__':
-    app.run()
+    main()
---
+++ runApi.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo nohup python api.py --prod &
