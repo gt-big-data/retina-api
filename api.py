0528c992cc310d8f1b411682242b374e5393e4c9
Philippe Laban <plaban3@gatech.edu>
2015-08-30 22:30:18 -0400

    Added dbco file

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from dbco import *
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/ask')
+def getRecentArticles():
+	return 'Miaw'
 
 if __name__ == '__main__':
     app.run()
