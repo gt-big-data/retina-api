2028030add5c6ead6ae083c039368125087c3611
bdc <bdc@bdc1>
2016-02-03 15:23:00 -0500

    Local

--- logger.py
+++ logger.py
@@ -2,5 +2,6 @@ from dbco import *
 import time
 
 def log(request):
+	print request.base_url
 	if 'localhost' not in request.base_url: # not if we are running locally, only for real requests
 		db.api_log.insert_one({'timestamp': int(time.time()), 'path': request.path, 'ip_addr': request.remote_addr})
